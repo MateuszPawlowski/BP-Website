667e6e359496132f199b8fe364f93af324497809
Mateusz Pawlowski <mateuszpawlowski@Mateuszs-MacBook-Pro.local>
2024-04-15 20:31:12 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import menu from './images/BPmenu.jpeg'
+function App() {
+  return (
+    <div>
+      <div className='text'>
+        <div className=''>The website is under construction.. Please comeback soon</div>
+        <hr></hr>
+        <div className='margin_btm flex'>
+          <div>
+              Checkout our socials <div><a className='link' href='https://www.instagram.com/bartollinipizza/?igshid=YmMyMTA2M2Y%3D'> 
+              Instagram</a> | <a className='link' href='https://www.facebook.com/people/Bartollini-Pizza/100088858897153/'>
+              Facebook
+            </a></div>
+          </div>
+          <div>Contact Us <div>0892063068</div></div>
+        </div>
+      </div>
+      <div className='imageDiv'>
+        <img className='image' src={menu} />
+      </div>
+      <div className='margin_top text'>
+        Our Location: H91 WC5P
+      </div>
+    </div>
+  );
+}
+
+export default App;
